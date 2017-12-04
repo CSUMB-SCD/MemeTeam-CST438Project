6eb0cc54ad38db96f5082b84bf2c48a314d1cd24
Sean McCarthy <smccarthy@csumb.edu>
2017-12-04 01:20:35 +0000

    Google initial commit

---
+++ public/javascripts/google-interactions.js
@@ -0,0 +1,42 @@
+function userLogin() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log ("Error code: "  + errorCode);
+        console.log("Error Message: " + errorMessage);
+        // ...
+    });
+}
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  
+  $("#logoutButton").toggle();
+}
+
+
+function signOut(){
+  
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    })
+    $("#logoutButton").toggle();
+
+}
