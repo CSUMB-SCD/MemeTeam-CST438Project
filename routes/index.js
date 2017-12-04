160390cf6581484c4f55069fe22fe0989f9280af
Laura <lmcrae@csumb.edu>
2017-12-04 02:05:18 +0000

    Changed some routing, and made event page as well as messages page. Messages page's javascript does not work as expected. Something is wrong with the javascript or css provided by bootstrap and jquery

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/profile', function(req, res) {
+  res.render('profile.jade', { title: 'Profile' });
+});
+
+router.get('/events', function(req, res, next) {
+  res.render('events.jade', { title: 'Events' });
+});
+
+router.get('/messages', function(req, res, next) {
+  res.render('chat.jade', { title: 'Chat' });
+});
+
 module.exports = router;
