81b5cc882b2fd88cd0331f2c8227a170477ba3d9
Christian Martinez <chrimartinez@csumb.edu>
2017-12-04 06:23:45 +0000

    Able to get values from forms in events page

---
+++ public/javascripts/createEvent.js
@@ -0,0 +1,14 @@
+function getInfo(){
+    
+    var title = document.getElementById('name').value;
+    var description = document.getElementById('description').value;
+    var location = document.getElementById('location').value;
+    var dateTime = document.getElementById('date').value;
+    
+    
+    
+    console.log(title);
+    console.log(description);
+    console.log(location);
+    console.log(dateTime);
+}
--- routes/event.js
+++ routes/event.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('event', { title: 'FUCKING WORKS' });
+  res.render('event', { title: 'Create Event' });
 });
 
 module.exports = router;
